377081e0643bbd67938d9a7e5602a7dc1b13e042
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-04-19 19:26:01 +0530

    Quick Sort Updated

--- Algorithms/Sorting/QuickSort.py
+++ Algorithms/Sorting/QuickSort.py
@@ -2,36 +2,45 @@
 
 
 
-def QuickSort(array:list, leftHandIterator:int, rightHandIterator:int)->list:
+def QuickSort(array:list, leftHandIterator:int, rightHandIterator:int)->None:
+    "while providing the righHandIterator which is basically highest or last element of list provide it as len(array) - 1"
+    if leftHandIterator < rightHandIterator:
+        # until leftHandIterator hasnt crossed or overlapped righthandIterator 
+        # partition the array in two subarray using partition method 
+        partitionPoint = Parition(array, leftHandIterator, rightHandIterator)
+
+        # apply quick sort on subarray left of the partition point
+        QuickSort(array, leftHandIterator, partitionPoint-1)
+        # apply quick sort on subarray right of the partition point
+        QuickSort(array, partitionPoint+1, rightHandIterator)
+
+def Swap(array:list, index_1:int, index_2:int)->None:
+    if index_1!=index_2:
+        aux = array[index_1]
+        array[index_1] = array[index_2]
+        array[index_2] = aux
+
+def Parition(array:list, leftHandIterator:int, rightHandIterator:int)->int:
+    # store initial pivotIndex and pivot element
+    pivotIndex = leftHandIterator
+    pivot = array[pivotIndex]
+
     while leftHandIterator < rightHandIterator:
-        partitionPoint = Partition(array, leftHandIterator, rightHandIterator)
-        QuickSort(array, leftHandIterator, partitionPoint - 1)
-        QuickSort(array, partitionPoint + 1, rightHandIterator)
-
-def Partition(array: list, leftHandIterator:int, rightHandIterator:int)->list:
-    pivot = array[0]
-
-    leftHandIterator +=1
-    rightHandIterator = rightHandIterator
-
-    done = False
-
-    while not done:
-        while leftHandIterator <= rightHandIterator and array[leftHandIterator] <= pivot:
+        # # until leftHandIterator hasnt crossed or overlapped righthandIterator
+        while leftHandIterator < rightHandIterator and array[leftHandIterator] <= pivot:
+            # if leftHandIterator has not crossed or overlapped the rightHandIterator
+            # and value at leftHandIterator is less or equal to pivot then keep moving leftHandIterator to the right
             leftHandIterator += 1
-        
-        while rightHandIterator >= leftHandIterator and array[rightHandIterator] >= pivot:
-                rightHandIterator -= 1
-        
-        if rightHandIterator < leftHandIterator:
-                done = True
-        else:
-            aux = array[leftHandIterator]
-            array[leftHandIterator] = array[rightHandIterator]
-            array[rightHandIterator] = aux
-    
+
+        while array[rightHandIterator] > pivot:
+            # value at rightHandIterator is greater than or equal to pivot then keep moving rightHandIterator to the left
+            rightHandIterator -= 1
+
+        if leftHandIterator < rightHandIterator:
+            # if leftHandIterator less than rightHandIterator and the value at leftHandIterator is bigger than pivot as well as value at rightHandIterator is smaller then pivot,  then swap the values at both indexes
+            Swap(array, leftHandIterator, rightHandIterator)
+
+    # now after swapping the values after the iterations we need to put the pivot at correct position by swapping it with the value at rightHandIterator as it is smaller than pivot
+    Swap(array, pivotIndex, rightHandIterator)
+
     return rightHandIterator
-
-
-a = [2,5,7,9,8, 3, 0]
-print(QuickSort(array=a, leftHandIterator=0, rightHandIterator=len(a)))
