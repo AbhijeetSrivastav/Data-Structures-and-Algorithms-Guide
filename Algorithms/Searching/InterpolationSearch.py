c9ea38c6a5e844fbbf11db22f15a5a3ad9a38209
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-04-20 12:52:25 +0530

    Interpolation Search Implementaion Completed

---
+++ Algorithms/Searching/InterpolationSearch.py
@@ -0,0 +1,20 @@
+"Interpolation Search Implementation(Upgraded Binary Search)"
+
+
+
+def InterpolationSearch(array:list, searchForValue:int)->int:
+    low = 0
+    high = len(array) - 1
+
+    while array[low] <= searchForValue  <= array[high]:
+        mid = (low + (searchForValue - array[low]) * (high - low)) // (array[high] - array[low])
+
+        if array[mid] > searchForValue:
+            high = mid - 1
+        elif array[mid] < searchForValue:
+            low = mid + 1
+        else:
+            # array[mid] == searchForValue
+            return mid
+    
+    return -1
