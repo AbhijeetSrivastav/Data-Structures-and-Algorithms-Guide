49de0aed27971ba5c1545ba7ae25de537d0a04bb
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-04-22 07:37:16 +0530

    Quadratic Probed Hash Table Implemented

---
+++ Data Structures/Hash Tables/QuadraticProbedHashTable.py
@@ -0,0 +1,45 @@
+"Hash Table - with Quadratic Probing collision resolution technique"
+
+
+
+class HashTable:
+    def __init__(self) -> None:
+        self.MAX = 256
+
+        # empty hash table with None value
+        self.array = [None for i in range(self.MAX)]
+
+
+    def getHash(self, key: str)->int:
+        # Generate hash value for given value or maps value to index
+        # hash = sum of the ASCII values of each char of key divided by the size of the hash table
+        # ord is method to get ASCII value for a charachter
+        hash = 0
+        for char in key:
+            hash += ord(char)
+        hash = hash % self.MAX
+        return hash
+
+    def __setitem__(self, key:str, value)->None:
+        # adds new value to hash table
+        # updates the value in the array at hash value for given key
+        h = self.getHash(key)
+
+        # iterator for quadratic function
+        k = 1
+        while self.array[h] is not None:
+            # if the slot at hashed value is already occupied
+            # rehash using the quadratic probe
+            # rehash(key) = (hash + k^2) % size_of_table    
+            h = (h + pow(k, 2)) % self.MAX
+        self.array[h] = value
+
+    def __getitem__(self, key:str):
+        # returns the value at given key
+        h = self.getHash(key)
+        return self.array[h]
+
+    def __delitem__(self, key:str)->None:
+        # deletes value for given key from the hash table
+        h = self.getHash(key)
+        self.array[h] = None
